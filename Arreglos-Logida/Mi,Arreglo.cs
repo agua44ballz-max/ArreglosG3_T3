b461a79c15ca6875440d2f5e88fbbd840659ac62
onza a secas <onzaasecas@gmail.com>
2026-09-17 08:22:20 -0600

    Versión_03: Método burbuja

--- Arreglos-Logida/Mi,Arreglo.cs
+++ Arreglos-Logida/Mi,Arreglo.cs
@@ -37,16 +37,54 @@ namespace Arreglos.Logica
 
 
         }
+        //Metodo ordenar
+        public void Ordenar()
+        {
+            for (int i = 0; i < _tope - 1; i++)
+            {
+                for (int j = i+1; j < _tope; j++)
+                {
+                    if (_arreglo[i] > _arreglo[j])
+                    {
+                        Cambiar(ref _arreglo[i], ref _arreglo[j]);
+                    }
+                }
+            }
+        }
+
+        //Método cambiar
+        public void Cambiar(ref int a, ref int b)
+        {
+
+            int aux = a;
+            a = b;
+            b = aux;
+
+        }
+
+        //Metodo ToString para mostrar el arreglo
         public override string ToString()
         {
             if (Estavacio)
             {
                 Console.WriteLine("El arreglo esta vacio");
             }
+
+            int contador = 0;
             string salida = string.Empty;
-            for (int i = 0; i < N; i++)
+            for (int i = 0; i < _tope; i++)
             {
+
                 salida += $"{_arreglo[i]}\t";
+                contador++;
+                if (contador > 9)
+                {
+                    contador = 0;
+                    //salida = salida + "\n";
+                    salida += "\n";
+                }
+                
+
             }
             return salida;
 
--- Arreglos.Ui.Consola/Program.cs
+++ Arreglos.Ui.Consola/Program.cs
@@ -3,12 +3,17 @@ using Arreglos.Logica;
 
 Console.WriteLine("Arreglos");
 
-MiArreglo oMiArreglo = new MiArreglo(10000);
+MiArreglo oMiArreglo = new MiArreglo(10);
 
-oMiArreglo.Llenar(5, 50);
+oMiArreglo.Llenar(1, 100);
 
+
+Console.WriteLine("Arreglo desordenado");
+Console.WriteLine(oMiArreglo);
 //oMyArreglo.ToString();
 
-Console.WriteLine(oMiArreglo.ToString());
+Console.WriteLine("Arreglo ordenado");
+oMiArreglo.Ordenar();
+Console.WriteLine(oMiArreglo);
 
 Console.ReadKey();
