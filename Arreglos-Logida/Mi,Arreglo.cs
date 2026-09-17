6c9ef8649881271a5e03fe4ff1ff341bb4170a6f
onza a secas <onzaasecas@gmail.com>
2026-09-17 07:27:41 -0600

    Versión_02: Método llenar arreglo

---
+++ Arreglos-Logida/Mi,Arreglo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arreglos.Logica
+{
+    public class MiArreglo
+    {
+        //Campos o atributos
+        private int _tope;
+        private int[] _arreglo;
+
+
+        // Constructor
+
+        public MiArreglo(int n)
+        {
+            N = n;
+            _arreglo = new int[n];
+            _tope = 0;
+        }
+        //Propiedades
+        public int N { get; }
+
+        public bool Esatalleno => _tope == N;
+        public bool Estavacio => _tope == 0;
+
+        //Metodos 
+        public void Llenar(int minimo, int maximo)
+        {
+            Random random = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                _arreglo[i] = random.Next(minimo, maximo);
+            }
+            _tope = N;
+
+
+        }
+        public override string ToString()
+        {
+            if (Estavacio)
+            {
+                Console.WriteLine("El arreglo esta vacio");
+            }
+            string salida = string.Empty;
+            for (int i = 0; i < N; i++)
+            {
+                salida += $"{_arreglo[i]}\t";
+            }
+            return salida;
+
+        }
+
+    }
+}
--- Arreglos.Ui.Consola/Program.cs
+++ Arreglos.Ui.Consola/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+using Arreglos.Logica;
+
+Console.WriteLine("Arreglos");
+
+MiArreglo oMiArreglo = new MiArreglo(10000);
+
+oMiArreglo.Llenar(5, 50);
+
+//oMyArreglo.ToString();
+
+Console.WriteLine(oMiArreglo.ToString());
+
+Console.ReadKey();
